d0c34abc29ba38e57680087a94f2df730c40676d
Stacey McKnight <mcknight.stm@gmail.com>
2021-11-07 10:42:20 -0600

    customize tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // Include path for any file that references any styles by name
+  purge: [
+    './public/*.html',
+    './public/*.js',
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
