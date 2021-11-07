09fe3281648f179f0e8e88032de33b4fcd7c5470
Stacey McKnight <mcknight.stm@gmail.com>
2021-11-07 14:36:50 -0600

    add primary and secondary colors to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
   ],
   darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgb(236, 72, 153)',
+        secondary: {
+          100: 'rgb(252, 231, 243)',
+          300: 'rgb(249, 168, 212)',
+        }
+      }
+    },
   },
   variants: {
     extend: {},
